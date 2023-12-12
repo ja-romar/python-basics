05590a180604be6311bd3375d0a974c55ad9f236
Sircule14 <victor.jromar@gmail.com>
2023-12-12 12:03:17 -0700

    Add two more attribtes of numpy

---
+++ Dev-Diary/Numpy/__pycache__/numpy_att.py
@@ -0,0 +1,16 @@
+#We create a new Numpu Array
+import  numpy as np
+
+exArr = np.array([1,2,3,4])
+
+#To know the size of the array
+print(exArr.size) #Return 4
+
+#to know the numbers of array dimensions
+print(exArr.ndim) #Return 1
+
+#To know the size of the array on each dimension
+#Return a tuple of integers
+print(exArr.shape)
+
+
