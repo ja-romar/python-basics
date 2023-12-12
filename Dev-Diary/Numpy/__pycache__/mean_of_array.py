3867babecc3d4ce51b251ecf5c9211d18aab2852
Sircule14 <victor.jromar@gmail.com>
2023-12-12 12:25:50 -0700

    get the mean of array with numpy

---
+++ Dev-Diary/Numpy/__pycache__/mean_of_array.py
@@ -0,0 +1,24 @@
+#Lets review some statical functions with Numpy
+
+#First we create a numpy Array
+import numpy as np
+
+
+a = np.array([1, -1, 1, -1])
+ 
+
+ # Get the mean (average) of numpy array
+mean = a.mean()
+print(mean) #return 0
+
+#Second example 
+
+numbers = np.array([10,15,20,25,30])
+
+numbersMean = numbers.mean()
+
+print(numbersMean)
+
+
+
+
