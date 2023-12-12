38f8c97548a3d069edb4f314c375e5e3d4594de6
Sircule14 <victor.jromar@gmail.com>
2023-12-12 13:48:29 -0700

    add array adition

---
+++ Dev-Diary/Numpy/__pycache__/array_addition.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+#First create an array
+u = np.array([1,0])
+
+print(u)
+
+#We crate a seccond array
+v = np.array([0,1])
+
+print(v)
+
+#We can add two arrays
+
+z = np.add(u, v)
+print(z)
+
+#this operation its equivalate to a vector add
