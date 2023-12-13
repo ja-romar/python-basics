e18dd56256c9a626aafb2feec5f5a58372709ad6
Sircule14 <victor.jromar@gmail.com>
2023-12-12 18:38:24 -0700

    add array division

---
+++ Dev-Diary/Numpy/__pycache__/array_division.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.array([10, 20, 30])
+print(a)
+
+b = np.array([2, 10, 5])
+print(b)
+
+#To divide two arrays...
+c = np.divide(a, b)
+print(c)
---
+++ Dev-Diary/Numpy/__pycache__/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+x=np.array([[1,0],[0,1]])
+y=np.array([[2,2],[2,2]])
+
+z=np.dot(x,y)
+
+print(z)
