23f874edf42576e899a6a03ca0f0576e1689d53e
Sircule14 <victor.jromar@gmail.com>
2023-12-12 19:07:18 -0700

    linspace

---
+++ Dev-Diary/Numpy/mathematical_functions/linspace.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+#linspace return evenly spaced numbers over a specified interval
+
+print(np.linspace(-2, 2, num=5))
+
+print(np.linspace(-10, 20, num=10))
--- Dev-Diary/Numpy/mathematical_functions/sin.py
+++ Dev-Diary/Numpy/mathematical_functions/sin.py
@@ -5,3 +5,6 @@ x = np.array([0, np.pi/2 , np.pi])
 y = np.sin(x)
 
 print(y)
+
+#plot the result 
+
