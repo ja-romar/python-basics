92d3b0c854225800572804c393bb700438877878
Sircule14 <victor.jromar@gmail.com>
2023-12-13 21:11:45 -0700

    commit

---
+++ scraping_pandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DA0321EN-SkillsNetwork/labs/datasets/HTMLColorCodes.html"
+
+#scrap tables from a webpage using pandas
+table = pd.read_html(url)
+print(table)
