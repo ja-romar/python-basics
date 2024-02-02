a8d1248e24aecd7013cb4b509b7bfaadf27cdec4
Sircule14 <victor.jromar@gmail.com>
2024-02-02 12:58:45 -0700

    change dataframe of dictionary

--- Dev-Diary/pandas/pandas1.py
+++ Dev-Diary/pandas/pandas1.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Creating a DataFrame from a dictionary
 data = {'Name': ['Alice', 'Bob', 'Charlie', 'David'],
         'Age': [25, 30, 35, 28],
-        'City': ['New York', 'San Francisco', 'Los Angeles', 'Chicago']}
+        'City': ['Monterrey', 'Chihuahua', 'CDMX', 'Sonora']}
 
 df = pd.DataFrame(data)
 
